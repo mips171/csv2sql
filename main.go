2e78d51439059a4f157d02f5c3747e542fc2b4ab
Nicholas Smith <nicholas@nbembedded.com>
2023-10-11 23:14:20 +1000

    sql dump format for size
    Signed-off-by: Nicholas Smith <nicholas@nbembedded.com>

--- main.go
+++ main.go
@@ -138,15 +138,12 @@ func orders() {
 	sqlFile.WriteString("TRUNCATE TABLE `oc_order_total`;\n")
 
 	// processTable(GetOrderTotalMapping(orderIDMapping), entities, orderIDMapping, sqlFile)
-	for _, record := range orders { // assuming orderRecords is a slice of OrderRecord
+	for _, record := range orders {
 		orderID := record.OrderID
 		subTotalValue, shippingCost, taxValue, totalValue := CalculateOrderTotals(record)
 
-		sqlStatements := GenerateOrderTotalSQLStatements(orderID, subTotalValue, shippingCost, taxValue, totalValue)
-
-		for _, stmt := range sqlStatements {
-			sqlFile.WriteString(stmt + "\n")
-		}
+		sqlStatement := GenerateOrderTotalSQLStatements(orderID, subTotalValue, shippingCost, taxValue, totalValue)
+		sqlFile.WriteString(sqlStatement + "\n")
 	}
 
 }
