630cce4f2b8ff3fe8b11a1bdf04427e05135243c
Nicholas Smith <nicholas@nbembedded.com>
2023-11-01 18:58:06 +1000

    optimise sql
    Signed-off-by: Nicholas Smith <nicholas@nbembedded.com>

--- sql_generator.go
+++ sql_generator.go
@@ -5,7 +5,9 @@ import (
 	"strings"
 )
 
-func GenerateInsertStatement(tableName string, columnOrder []string, entities []Entity, fields []FieldMapping, uniqueIdentifier string, uniqueIdMapping map[string]int) []string {
+const batchSize = 10000
+
+func GenerateInsertStatement(tableName string, columnOrder []string, entities []Entity, fields []FieldMapping) []string {
 	var statements []string
 
 	// Create a map for faster look-up
@@ -14,33 +16,41 @@ func GenerateInsertStatement(tableName string, columnOrder []string, entities []
 		fieldMap[field.DBColumnName] = field
 	}
 
-	for _, entity := range entities {
-		var rowValues []string
+	backtickedColumns := make([]string, len(columnOrder))
+	for i, col := range columnOrder {
+		backtickedColumns[i] = fmt.Sprintf("`%s`", col)
+	}
 
-		for _, col := range columnOrder {
-			if field, exists := fieldMap[col]; exists {
-				value := field.MappingFunction(entity)
-				switch v := value.(type) {
-				case string:
-					// replace all \ with ""
-					v = strings.ReplaceAll(v, "\\", "")
-					rowValues = append(rowValues, fmt.Sprintf("'%s'", strings.ReplaceAll(v, "'", "''")))
-				case int:
-					rowValues = append(rowValues, fmt.Sprintf("%d", v))
-				default:
-					// Handle other types or raise an error if needed
+	for i := 0; i < len(entities); i += batchSize {
+		var allValues []string
+		end := i + batchSize
+
+		if end > len(entities) {
+			end = len(entities)
+		}
+
+		for j := i; j < end; j++ {
+			var rowValues []string
+			for _, col := range columnOrder {
+				if field, exists := fieldMap[col]; exists {
+					value := field.MappingFunction(entities[j])
+					switch v := value.(type) {
+					case string:
+						v = strings.ReplaceAll(v, "\\", "")
+						rowValues = append(rowValues, fmt.Sprintf("'%s'", strings.ReplaceAll(v, "'", "''")))
+					case int:
+						rowValues = append(rowValues, fmt.Sprintf("%d", v))
+					default:
+						// Handle other types or raise an error if needed
+					}
+				} else {
+					rowValues = append(rowValues, "NULL") // Handle fields not present in the CSV
 				}
-			} else {
-				rowValues = append(rowValues, "NULL") // Handle fields not present in the CSV
 			}
+			allValues = append(allValues, fmt.Sprintf("(%s)", strings.Join(rowValues, ", ")))
 		}
 
-		backtickedColumns := make([]string, len(columnOrder))
-		for i, col := range columnOrder {
-			backtickedColumns[i] = fmt.Sprintf("`%s`", col)
-		}
-
-		statement := fmt.Sprintf("INSERT IGNORE INTO `%s` (%s) VALUES (%s);", tableName, strings.Join(backtickedColumns, ", "), strings.Join(rowValues, ", "))
+		statement := fmt.Sprintf("INSERT IGNORE INTO `%s` (%s) VALUES\n%s;", tableName, strings.Join(backtickedColumns, ", "), strings.Join(allValues, ",\n"))
 		statements = append(statements, statement)
 	}
 
